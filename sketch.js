2dd44a03557a234ce777f0c091f023237fc93821
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-25 21:54:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,89 +1,75 @@
-var database ,dog,dog1,dog2
-var position
-//var form
-var feed,add
-var foodobject
-var Feedtime
-var Lastfeed
-//Create variables here
+const db = firebase.database();
+var dog, happydDog, foodS, foodStock;
+var stockbutton
+var dogimg, happydogimg;
+var food
+var hour
+var period = ""
 
-function preload()
-
-{
-  dogimg1 = loadImage("images/dogImg.png")
-  dogimg2 = loadImage("images/dogImg1.png")
-	//load images here
+function preload(){
+  
+  dogimg = loadImage("images/dogImg.png");
+  dogimg2 = loadImage("images/dogImg1.png");
 }
 
 function setup() {
-	createCanvas(1000, 500);
-  database = firebase.database();
-  console.log(database);
- 
-  foodobject=new Food()
-  dog = createSprite(550,250,10,10);
-  dog.addImage(dogimg1)
-  dog.scale=0.2
- 
-  var dogo = database.ref('Food');
-  dogo.on("value", readPosition, showError);
-  feed = createButton("FEED DRAGO")
-  feed.position(500,15)
-  feed.mousePressed(FeedDog)
-  add = createButton("ADD FOOD")
-  add.position(400,15)
-  add.mousePressed(AddFood)
+  food = new Food;
+  food.getFS
+  canvas = createCanvas(800, 800);
+  dog = createSprite (575, 250,1,1);
+  dog.scale = 0.5;
+  dog.addImage(dogimg, "dogimg");
+
+  stockbutton = createButton("Add Stock");
+  stockbutton.mousePressed(stock);
+  stockbutton.position(100,100)
+  
+  feadbutton = createButton("Feed");
+  feadbutton.mousePressed(feed);
+  feadbutton.position(200,100)
 
 } 
-
-function draw(){
- background(46,139,87);
-
- foodobject.display()
  
- drawSprites();
+
+function draw() {  
+  background(46, 139, 87);
+  drawSprites();
+  food.getFS();
+  food.getLF();
+
+  getFeedTime();
+
+ noStroke();
+ textSize(35)
+ fill("blue")
+  text("Food Remaing: "+ food.foodStock, 50, 120);
   
- fill(255,255,254);
- textSize(15);
-
-drawSprites();
+  text("Last Fed: "+ food.lastFed, 50, 80 )
+  //text("Remember the Up arrow, will feed the dog.",50, 150);
+  food.display();
+function stock(){
+  food.updateFS();
+  
+  console.log("It be working")
 }
-function readPosition(data){
-  position = data.val();
-  foodobject.updateFoodStock(position)
-}
-
-function showError(){
-  console.log("Error in writing to the database");
-}
-
-function writePosition(nazo){
-  if(nazo>0){
-    nazo=nazo-1
+function feed(){
+  food.deductF();
+  food.updateLF()
+  if(hour > 12){
+      period = "PM"
   }
-  else{
-    nazo=0
+  if(hour < 12){
+      period = "AM"
   }
-  database.ref('/').set({
-    'Food': nazo
-  })
+  console.log("It be working")
 
 }
-function AddFood(){
-position++
-database.ref('/').update({
-  Food:position
-}
+async function getFeedTime(){
+  var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/kolkata");
+  var responseJSON = await response.json();
 
-)
-}
-function FeedDog(){
-
-dog.addImage(dogimg2)
-foodobject.updateFoodStock(foodobject.getFoodStock()-1)
- database.ref('/').update({
-   Food:foodobject.getFoodStock(),
-   FeedTime:hour ()
- })
-
+  var datetime = responseJSON.datetime;
+  hour = datetime.slice(11,13);
+  
+  console.log(hour)
 }
