11bae6ae966f2e040f52815d102822aaa7c185e9
aditineemkar <74664973+aditineemkar@users.noreply.github.com>
2021-01-25 21:55:55 +0530

    Update Food.js

--- Js/Food.js
+++ Js/Food.js
@@ -1,38 +1,55 @@
 class Food {
-  constructor(){
-  this.foodStock=0;
-  this.image=loadImage('images/Milk.png');
-  }
+    constructor() {
+       this.foodStock = null
+       this.lastFed = null
+       this.bottleimg = loadImage("images/Milk.png")
+    }
 
- updateFoodStock(foodStock){
-  this.foodStock=foodStock;
+    getFS(){
+       var foodSref = db.ref('/Food')
+       foodSref.on("value", (data) =>{
+       this.foodStock = data.val(); 
+     })
+  }
+     
+    getLF(){
+       var foodSref = db.ref('/LastFedTime')
+       foodSref.on("value", (data) =>{
+       this.lastFed = data.val(); 
+     })
+  } 
+
+  updateLF(){
+    var foodSref = db.ref('/') 
+    foodSref.update({
+        'LastFedTime': hour + period
+
+    })
  }
 
- deductFood(){
-   if(this.foodStock>0){
-    this.foodStock=this.foodStock-1;
-   }
+deductF(){
+    var foodSrefs = db.ref('/') 
+    foodSrefs.update({
+        'Food': this.foodStock - 1
+    })
   }
-
-  getFoodStock(){
-    return this.foodStock;
-  }
-
   display(){
-    var x=80,y=100;
-    
+    var x = 80
+    var y = 100
     imageMode(CENTER);
- 
-    
-    if(this.foodStock!=0){
-      for(var i=0;i<this.foodStock;i++){
-        if(i%10==0){
-          x=80;
-          y=y+50;
+    if(this.foodStock > 0){
+        
+        for(var i = 0; i < this.foodStock; i++){
+              if(i%10 === 0){
+                x = 60
+                y = y + 60
+            }
+            
+            image(this.bottleimg, x, y , 60, 55)
+            x = x + 30
         }
-        image(this.image,x,y,50,50);
-        x=x+30;
-      }
     }
-  }
-}
+    
+    }
+    
+} 
